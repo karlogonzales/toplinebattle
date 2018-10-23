ae4d60a8a5e94180d62cf15366a9ce0997135973
Andy <andy.pham2956@gmail.com>
2018-10-23 15:44:17 -0400

    added seb + update teams

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,45 @@
 var express = require('express');
 var router = express.Router();
 const axios = require("axios");
-const seguin = "http://api.eliteprospects.com/beta/players/23807?apikey=6c55096112cbbae549d608238c861ddf";
-const benn = "http://api.eliteprospects.com/beta/players/15399?apikey=6c55096112cbbae549d608238c861ddf";
-const radulov = "http://api.eliteprospects.com/beta/players/8794?apikey=6c55096112cbbae549d608238c861ddf";
+
+const mackinnon = "http://api.eliteprospects.com/beta/players/99204?apikey=6c55096112cbbae549d608238c861ddf";
+const rantanen = "http://api.eliteprospects.com/beta/players/91186?apikey=6c55096112cbbae549d608238c861ddf";
+const landeskog = "http://api.eliteprospects.com/beta/players/10393?apikey=6c55096112cbbae549d608238c861ddf";
+
 const scheifele = "http://api.eliteprospects.com/beta/players/75379?apikey=6c55096112cbbae549d608238c861ddf";
 const wheeler = "http://api.eliteprospects.com/beta/players/9215?apikey=6c55096112cbbae549d608238c861ddf";
-const laine = "http://api.eliteprospects.com/beta/players/221667?apikey=6c55096112cbbae549d608238c861ddf";
+const connor = "http://api.eliteprospects.com/beta/players/177671?apikey=6c55096112cbbae549d608238c861ddf";
+
+const bergeron = "http://api.eliteprospects.com/beta/players/9079?apikey=6c55096112cbbae549d608238c861ddf";
+const marchand = "http://api.eliteprospects.com/beta/players/10439?apikey=6c55096112cbbae549d608238c861ddf";
+const pastrnak = "http://api.eliteprospects.com/beta/players/130383?apikey=6c55096112cbbae549d608238c861ddf";
+
 
 let karloTotalPoints = 0;
 let andyTotalPoints = 0;
+let sebTotalPoints = 0;
 
-let seguinG = seguinA = seguinPt = 0;
-let bennG = bennA = bennPt = 0;
-let radulovG = radulovA = radulovPt = 0;
+let mackinnonG = mackinnonA = mackinnonPt = 0;
+let rantanenG = rantanenA = rantanenPt = 0;
+let landeskogG = landeskogA = landeskogPt = 0;
 
 let scheifeleG = scheifeleA = scheifelePt = 0;
 let wheelerG = wheelerA = wheelerPt = 0;
-let laineG = laineA = lainePt = 0;
+let connorG = connorA = connorPt = 0;
+
+let bergeronG = bergeronA = bergeronPt = 0;
+let marchandG = wheelerA = wheelerPt = 0;
+let pastrnakG = connorA = connorPt = 0;
 
 
-axios.get(seguin)
+axios.get(mackinnon)
     .then(response => {
         var goals = parseInt(response.data.data.latestPlayerStats.G);
         var assists = parseInt(response.data.data.latestPlayerStats.A);
-        karloTotalPoints += goals + assists;
-        seguinG = goals;
-        seguinA = assists;
-        seguinPt = seguinG+seguinA;
+        andyTotalPoints += goals + assists;
+        mackinnonG = goals;
+        mackinnonA = assists;
+        mackinnonPt = mackinnonG+mackinnonA;
 
         console.log(
             `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
@@ -37,14 +49,14 @@ axios.get(seguin)
         console.log(error);
     });
 
-axios.get(benn)
+axios.get(rantanen)
     .then(response => {
         var goals = parseInt(response.data.data.latestPlayerStats.G);
         var assists = parseInt(response.data.data.latestPlayerStats.A);
-        karloTotalPoints += goals + assists;
-        bennG = goals;
-        bennA = assists;
-        bennPt = bennG+bennA;
+        andyTotalPoints += goals + assists;
+        rantanenG = goals;
+        rantanenA = assists;
+        rantanenPt = rantanenG+rantanenA;
         console.log(
             `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
         );
@@ -53,14 +65,14 @@ axios.get(benn)
         console.log(error);
     });
 
-axios.get(radulov)
+axios.get(landeskog)
     .then(response => {
         var goals = parseInt(response.data.data.latestPlayerStats.G);
         var assists = parseInt(response.data.data.latestPlayerStats.A);
-        karloTotalPoints += goals + assists;
-        radulovG = goals;
-        radulovA = assists;
-        radulovPt = radulovG+radulovA;
+        andyTotalPoints += goals + assists;
+        landeskogG = goals;
+        landeskogA = assists;
+        landeskogPt = landeskogG+landeskogA;
         console.log(
             `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
         );
@@ -76,7 +88,7 @@ axios.get(scheifele)
         scheifeleG = goals;
         scheifeleA = assists;
         scheifelePt = scheifeleG+scheifeleA;
-        andyTotalPoints += goals + assists;
+        karloTotalPoints += goals + assists;
         console.log(
             `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
         );
@@ -89,7 +101,7 @@ axios.get(wheeler)
     .then(response => {
         var goals = parseInt(response.data.data.latestPlayerStats.G);
         var assists = parseInt(response.data.data.latestPlayerStats.A);
-        andyTotalPoints += goals + assists;
+        karloTotalPoints += goals + assists;
         wheelerG = goals;
         wheelerA = assists;
         wheelerPt = wheelerG+wheelerA;
@@ -101,14 +113,61 @@ axios.get(wheeler)
         console.log(error);
     });
 
-axios.get(laine)
+axios.get(connor)
     .then(response => {
         var goals = parseInt(response.data.data.latestPlayerStats.G);
         var assists = parseInt(response.data.data.latestPlayerStats.A);
-        andyTotalPoints += goals + assists;
-        laineG = goals;
-        laineA = assists;
-        lainePt = laineG+laineA;
+        karloTotalPoints += goals + assists;
+        connorG = goals;
+        connorA = assists;
+        connorPt = connorG+connorA;
+        console.log(
+            `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
+        );
+    })
+    .catch(error => {
+        console.log(error);
+    });
+axios.get(bergeron)
+    .then(response => {
+        var goals = parseInt(response.data.data.latestPlayerStats.G);
+        var assists = parseInt(response.data.data.latestPlayerStats.A);
+        bergeronG = goals;
+        bergeronA = assists;
+        bergeronPt = bergeronG+bergeronA;
+        sebTotalPoints += goals + assists;
+        console.log(
+            `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
+        );
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+axios.get(marchand)
+    .then(response => {
+        var goals = parseInt(response.data.data.latestPlayerStats.G);
+        var assists = parseInt(response.data.data.latestPlayerStats.A);
+        sebTotalPoints += goals + assists;
+        marchandG = goals;
+        marchandA = assists;
+        marchandPt = marchandG+marchandA;
+        console.log(
+            `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
+        );
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+axios.get(pastrnak)
+    .then(response => {
+        var goals = parseInt(response.data.data.latestPlayerStats.G);
+        var assists = parseInt(response.data.data.latestPlayerStats.A);
+        sebTotalPoints += goals + assists;
+        pastrnakG = goals;
+        pastrnakA = assists;
+        pastrnakPt = pastrnakG+pastrnakA;
         console.log(
             `Goals: ${goals} \nAssists: ${assists}\nTotal Points: ${goals + assists}`
         );
@@ -121,16 +180,17 @@ router.get('/', function (req, res, next) {
     res.render('index', {   title: 'Top Line Battle',
                             karloTotalPoints: karloTotalPoints,
                             andyTotalPoints: andyTotalPoints,
+                            sebTotalPoints: sebTotalPoints,
 
-                            seguinG: seguinG,
-                            seguinA: seguinA,
-                            seguinPt: seguinPt,
-                            bennG: bennG,
-                            bennA: bennA,
-                            bennPt: bennPt,
-                            radulovG: radulovG,
-                            radulovA: radulovA,
-                            radulovPt: radulovPt,
+                            mackinnonG: mackinnonG,
+                            mackinnonA: mackinnonA,
+                            mackinnonPt: mackinnonPt,
+                            rantanenG: rantanenG,
+                            rantanenA: rantanenA,
+                            rantanenPt: rantanenPt,
+                            landeskogG: landeskogG,
+                            landeskogA: landeskogA,
+                            landeskogPt: landeskogPt,
 
                             scheifeleG: scheifeleG,
                             scheifeleA: scheifeleA,
@@ -138,9 +198,19 @@ router.get('/', function (req, res, next) {
                             wheelerG: wheelerG,
                             wheelerA: wheelerA,
                             wheelerPt: wheelerPt,
-                            laineG: laineG,
-                            laineA: laineA,
-                            lainePt: lainePt
+                            connorG: connorG,
+                            connorA: connorA,
+                            connorPt: connorPt,
+
+                            bergeronG: bergeronG,
+                            bergeronA: bergeronA,
+                            bergeronPt: bergeronPt,
+                            marchandG: marchandG,
+                            marchandA: marchandA,
+                            marchandPt: marchandPt,
+                            pastrnakG: pastrnakG,
+                            pastrnakA: pastrnakA,
+                            pastrnakPt: pastrnakPt
     });
 });
 
