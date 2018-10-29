73bb807fa65a10ac5371dac7786375575fbbfdf9
Andy <andy.pham2956@gmail.com>
2018-10-29 11:39:04 -0400

    crud operations to timcard controller

--- controllers/timcard.controller.js
+++ controllers/timcard.controller.js
@@ -6,9 +6,28 @@ exports.timcard_create = function(req, res){
         playername: req.body.playername
     });
     timcard.save(function(err){
-        if(err){
-            return next(err);
-        }
+        if(err)return next(err);
         res.send('Tim Card successfully created!');
     });
+};
+
+exports.timcard_read = function(req, res){
+    Timcard.findById(req.params.id, function(err, timcard){
+        if(err) return next(err);
+        res.send(timcard);
+    });
+};
+
+exports.timcard_update = function(req, res){
+  Timcard.findByIdAndUpdate(req.params.id, {$set: req.body}, function(err, timcard){
+      if(err) return next(err);
+      res.send('Tim card updated!')
+  })
+};
+
+exports.timcard_delete = function(req, res){
+  Timcard.findByIdAndRemove(req.params.id, function(err){
+      if(err) return next(err);
+      res.send('Tim card deleted!')
+  })
 };
--- routes/timcard.route.js
+++ routes/timcard.route.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const timcard_controller = require('../controllers/timcard.controller');
 
 router.post('/create', timcard_controller.timcard_create);
+router.get('/:id', timcard_controller.timcard_read);
+router.put('/:id/update', timcard_controller.timcard_update);
+router.delete('/:id/delete', timcard_controller.timcard_delete);
 
 module.exports = router;
